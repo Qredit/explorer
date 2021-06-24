13aa5941a5c401094f22988c7da669b2bd88a76e
Nayiem <info@hodlercompany.com>
2021-06-24 23:08:25 +0200

    changes for ark network

--- src/services/qreditslt.ts
+++ src/services/qreditslt.ts
@@ -7,35 +7,35 @@ import store from '@/store'
 class QreditSltService {
     async getTransaction(transactionid) {
         const response = await axios
-            .get(`https://qslp.qredit.cloud/api/transaction/${transactionid}`)
+            .get(`https://arkqslp.qredit.cloud/api/transaction/${transactionid}`)
 
         return response.data // .transform(response.data.Data, dateTimeFormat)
     }
 
     async getToken(tokenid) {
         const response = await axios
-            .get(`https://qslp.qredit.cloud/api/token/${tokenid}`)
+            .get(`https://arkqslp.qredit.cloud/api/token/${tokenid}`)
 
         return response.data; // .transform(response.data.Data, dateTimeFormat)
     }
 
     async getTokens() {
         const response = await axios
-            .get(`https://qslp.qredit.cloud/api/tokens`)
+            .get(`https://arkqslp.qredit.cloud/api/tokens`)
 
         return response.data // .transform(response.data.Data, dateTimeFormat)
     }
 
     async getWalletTokens(walletid) {
         const response = await axios
-            .get(`https://qslp.qredit.cloud/api/address/${walletid}`)
+            .get(`https://arkqslp.qredit.cloud/api/address/${walletid}`)
 
         return response.data // .transform(response.data.Data, dateTimeFormat)
     }
 
     async getTransactions(tokenid) {
         const response = await axios
-            .get(`https://qslp.qredit.cloud/api/transactions/${tokenid}`)
+            .get(`https://arkqslp.qredit.cloud/api/transactions/${tokenid}`)
 
         return response.data // .transform(response.data.Data, dateTimeFormat)
     }
